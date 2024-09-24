f6e2d3db1461e2ee2b3363b20b69daa4bbc8317a
kartikey701 <166546765+kartikey701@users.noreply.github.com>
2024-09-25 00:36:48 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,39 +1,10 @@
-'''from flask import Flask,redirect,url_for,jsonify,render_template,request
-import pickle
-import numpy as np
-
-app=Flask(__name__)
-model = pickle.load(open('randomforest_acute_myocardial_infarction.pkl','rb'))
-
-@app.route('/')  #iss page rr jaunga toh ye def wel wala function trigger ho jayega
-def home():
-    return render_template('index.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-    float_features  = [int(x) for x in request.form.values()]
-    final_features = [np.array(float_features)]
-    prediction = model.predict(final_features)
- 
-    return render_template('index.html',prediction_text = 'it is  {}'.format(prediction))
-
-
-@app.route('/predict_api',methods = ['POST'])
-def predict_api():
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-    
-    output = prediction[0]
-    return jsonify(output)
-
-if __name__ == '__main__':
-    app.run(debug = True)'''
-
-from flask import Flask, redirect, url_for, jsonify, render_template, request
+from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
 app = Flask(__name__)
+
+# Load your trained model (make sure the path is correct)
 model = pickle.load(open('randomforest_acute_myocardial_infarction.pkl', 'rb'))
 
 @app.route('/')
@@ -42,30 +13,29 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Convert form values to integers
-    float_features = [int(x) for x in request.form.values()]
-    
-    # Log the int_features variable
-    print("int_features:", float_features)
+    try:
+        # Collecting input values from the form
+        float_features = [float(x) for x in request.form.values()]
+        final_features = np.array([float_features])
 
-    # Convert to numpy array
-    final_features = [np.array(float_features)]
-    
-    # Make prediction
-    prediction = model.predict(final_features)
- 
-    return render_template('index.html', prediction_text='The prediction is {}'.format(prediction))
+        # Make prediction using the loaded model
+        prediction = model.predict(final_features)
 
-@app.route('/predict_api', methods=['POST'])
-def predict_api():
-    # Get JSON data from request
-    data = request.get_json(force=True)
-    
-    # Make prediction
-    prediction = model.predict([np.array(list(data.values()))])
-    
-    # Return prediction as JSON
-    return jsonify(prediction[0])
+        # Check the prediction result (1 or 0)
+        result = prediction[0]
+
+        # Prepare the result and advice based on prediction
+        if result == 1:
+            advice = "Need to visit dr."
+        else:
+            advice = "All fine."
+
+        # Redirect to a new page with the result and advice
+        return render_template('result.html', result=result, advice=advice)
+
+    except Exception as e:
+        # If something goes wrong, print the error and return an error page or message
+        return f"An error occurred: {str(e)}"
 
 if __name__ == '__main__':
     app.run(debug=True)
